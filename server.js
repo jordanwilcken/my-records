18bc06abb0aae6ab9e20ec624cdf17441732f03d
Jordan Wilcken <jordanwilcken@gmail.com>
2016-09-28 22:04:32 -0600

    Think if got the auth and password things working

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 'use strict';
 
 var
+  config = require('config'),
+  qPromises = require('q'),
+  bcrypt = require('bcrypt-nodejs'),
+  webToken = require('jsonwebtoken'),
+  uuid = require('node-uuid'),
   http    = require( 'http'     ),
   express = require( 'express'  ),
   logger = require('morgan'),
@@ -12,12 +17,48 @@ var
   port = process.env.PORT || 3000,
   app     = express(),
   router = express.Router(),
-  server  = http.createServer( app );
+  server  = http.createServer( app ),
+  authTokens = [];
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(router);
 router.use(logger('combined'));
+
+app.post('/login', function(request, response) {
+  var newAuthToken;
+
+  if (!bcrypt.compareSync(request.body.password, config.get('password'))) {
+    response.status(401).send("invalid credentials");
+    return;
+  }
+
+  newAuthToken = webToken.sign({ uuid: uuid.v1() }, config.get('secret'), { expiresIn: '2h' });
+  authTokens.push(newAuthToken); 
+  response.send(newAuthToken);
+});
+
+function checkAuthenticated(request, response, next) {
+  var
+    authToken = (function(authHeader) {
+      var
+        regexData = /Bearer (.+)$/.exec(authHeader);
+
+	  return regexData && regexData.length > 1
+        ? regexData[1]
+        : null;
+    }(request.get('Authorization'))),
+
+    authTokenNotInList = authTokens.indexOf(authToken) === -1;
+
+  if (authTokenNotInList) {
+    response.status(401).send('You are not authenticated');
+  } else {
+    next();
+  }
+}
+
+app.all('*', checkAuthenticated);
 
 app.get('/:id', function(request, response) {
   function returnData(data) {
@@ -35,9 +76,6 @@ app.get('/:id', function(request, response) {
 
   recordRepo.get(request.params.id)
     .then(returnData, handleError);
-});
-
-app.post('/', function(req, res) {
 });
 
 // ----------------- BEGIN START SERVER -------------------
